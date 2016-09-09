9e24be2a6aa20de0a5b45447da3c8b9964bca67f
EC2 Default User <ec2-user@ip-10-0-0-232.eu-west-1.compute.internal>
2016-09-09 02:19:26 +0000

    Created installRICEW_setParams.sh

---
+++ branches/DEVELOPMENT/jenkins/bin/installRICEW_setParams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+usage(){
+  echo "$0 [ -g <Generate Master Files Only value = true | false> ] [ -p <Protocol value = http | https> ]"
+  exit 1
+}
+if [ -z "${g}" ] || [ -z "${p}" ]
+then
+  usage
+else
+  while getopts ":g:p:" opt; do
+    case "${opt}" in
+      g) g=${OPTARG};;
+      p) p=${OPTARG};;
+    esac
+  done
+fi
+bamboo.GENERATE_MASTER_FILES_ONLY=$g
+bamboo.PROTOCOL=$p
+
